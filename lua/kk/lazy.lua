bf059f3a7ae2d06c00c09542a07db9155f365def
KK <82811712+KarthikKaravatt@users.noreply.github.com>
2024-11-26 13:00:57 +0800

    Add tailwind tools

--- lua/kk/lazy.lua
+++ lua/kk/lazy.lua
@@ -84,6 +84,25 @@ local plugins = {
     opts = {},
   },
 
+  -- Tailwind tools
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {} -- your configuration
+  },
+  {
+    'razak17/tailwind-fold.nvim',
+    opts= {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    ft = { 'html', 'svelte', 'astro', 'vue', 'typescriptreact', 'php', 'blade' },
+  },
+
   -- LSP and completion
   { 'neovim/nvim-lspconfig' },
   { 'hrsh7th/cmp-nvim-lsp' },
